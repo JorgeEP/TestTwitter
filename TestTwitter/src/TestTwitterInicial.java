4ade2f4a72cd601f28d54e4676bf81d4389a0e35
JorgeEP <JorgeEP@gmail.com>
2017-04-04 13:54:47 -0500

    TestTwitterInicial - contiene la pregunta del token que genera la autenticación de twitter.
    TestTwitter - Usa las claves generadas en la autenticación.

--- TestTwitter/src/TestTwitterInicial.java
+++ TestTwitter/src/TestTwitterInicial.java
@@ -3,21 +3,38 @@ import twitter4j.auth.*;
 import java.io.*;
 
 
-public class TestTwitter {
+public class TestTwitterInicial {
     static String CONSUMER_KEY        = "tM4ZCfnkB8waqawxolJ7CHiP3";
     static String CONSUMER_KEY_SECRET = "Yv6ZySZSyGimcU8kcHKbjfcFSnWgtelxkooSGo59Y8eCPkczCl";
     
     public static void main(String[] args) throws Exception {
         Twitter twitter = new TwitterFactory().getInstance();
         twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_KEY_SECRET);
-        
-        // here's the difference
-        String accessToken       = "161307183-Q1DNQGFiHyRq7uICC1oyp7PJSGkHTiCK1U1TLMmB";
-        String accessTokenSecret = "Fmaxi99M4a1MSQZ3uu3lvJsAh4FZP8RJIG9ufldkIz2Hh";
-        AccessToken oathAccessToken = new AccessToken(accessToken, accessTokenSecret);
-        twitter.setOAuthAccessToken(oathAccessToken);
-        // end of difference
-        
+
+        RequestToken requestToken = twitter.getOAuthRequestToken();
+        System.out.println("Authorization URL: \n"
+                + requestToken.getAuthorizationURL());
+
+        AccessToken accessToken = null;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.print("Hit above Authorization URL and Input PIN here: ");
+            String pin = br.readLine();
+
+            accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+
+        } catch (TwitterException te) {
+
+            System.out.println("Failed to get access token, caused by: "
+                    + te.getMessage());
+        }
+
+        System.out.println("Access Token: " + accessToken.getToken());
+        System.out.println("Access Token Secret: "
+                + accessToken.getTokenSecret());
+
         // updating twitter status
         twitter.updateStatus("hi.. im updating this using Namex Tweet for Demo");
 
